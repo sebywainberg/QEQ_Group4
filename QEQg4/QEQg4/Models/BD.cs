7a7f2fb76b1bf6866264c0765958fc18d9e3a730
LEONEL MATIAS GOLDENBERG <43871489@comunidadort.edu.ar>
2018-10-16 12:10:44 -0300

    primera clase

---
+++ QEQg4/QEQg4/Models/BD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace QEQg4.Models
+{
+    public static class BD
+    {
+        public static string connectionString = "sever=10.128.8.16;Database=QEQC04;User ID=QEQC04;pwd=QEQC04";
+        private static SqlConnection conectar()
+        {
+            SqlConnection conex = new SqlConnection(connectionString);
+          conex.Open();
+          return conex;
+        }
+
+        public static string loginUsuario(string Username, string password)
+        {
+            string Devuelve = "";
+            SqlConnection conex = conectar();
+            SqlCommand Consulta = conex.CreateCommand();
+            Consulta.CommandText = "LoginUsuario";
+            Consulta.CommandType = System.Data.CommandType.StoredProcedure;
+            Consulta.Parameters.AddWithValue("@username", Username);
+            Consulta.Parameters.AddWithValue("@password", password);
+            SqlDataReader dataReader = Consulta.ExecuteReader();
+            if(dataReader.Read())
+            {
+                Devuelve = dataReader["username"].ToString();
+            }
+            desconectar(conex);
+            return Devuelve;
+        }
+        public static void desconectar( SqlConnection conex)
+        {
+            conex.Close();
+        }
+    }
+}
--- QEQg4/QEQg4/Models/usuario.cs
+++ QEQg4/QEQg4/Models/usuario.cs
@@ -14,11 +14,34 @@ namespace QEQg4.Models
         private int _puntos;
         private bool _EsAdmin;
 
+ 
+
         public int Id { get => _id; set => _id = value; }
         public string Username { get => _username; set => _username = value; }
         public string Password { get => _password; set => _password = value; }
         public string Nombre { get => _Nombre; set => _Nombre = value; }
         public int Puntos { get => _puntos; set => _puntos = value; }
         public bool EsAdmin { get => _EsAdmin; set => _EsAdmin = value; }
+
+        public usuario(int idd, string user, string pass, string Nomb, int punto, bool EsAdm)
+        {
+            _id = idd;
+            _username = user;
+            _password = pass;
+            _Nombre = Nomb;
+            _puntos = punto;
+            _EsAdmin = EsAdm;
+        }
+
+        public usuario()
+        {
+            _id = 0;
+            _username = "";
+            _password = "";
+            _Nombre = "";
+            _puntos = 0;
+            _EsAdmin = false;
+        }
     }
+
 }
