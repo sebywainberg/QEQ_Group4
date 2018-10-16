59dc011a7da8bd12d5c7ff300ae296d815ced164
LEONEL MATIAS GOLDENBERG <43871489@comunidadort.edu.ar>
2018-10-16 10:07:58 -0300

    Primer Commit

---
+++ QEQg4/QEQg4/Models/usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QEQg4.Models
+{
+    public class usuario
+    {
+        private int _id;
+        private string _username;
+        private string _password;
+        private string _Nombre;
+        private int _puntos;
+        private bool _EsAdmin;
+
+        public int Id { get => _id; set => _id = value; }
+        public string Username { get => _username; set => _username = value; }
+        public string Password { get => _password; set => _password = value; }
+        public string Nombre { get => _Nombre; set => _Nombre = value; }
+        public int Puntos { get => _puntos; set => _puntos = value; }
+        public bool EsAdmin { get => _EsAdmin; set => _EsAdmin = value; }
+    }
+}
